e6d2b0c47d2945e15a00cf067c8bce88b23b5556
Kurt Heimbigner <10818122@uvu.edu>
2019-01-24 14:01:34 -0700

    Adding images and the background of the header.

---
+++ JS/main.js
@@ -0,0 +1,6 @@
+// JavaScript Document
+
+///Adding in the header text
+
+document.querySelector('#headerOne').textContent = "Joe's Bed and Breakfast";
+document.querySelector('#headerTwo').textContent = "Breakfast is Better in Bed";
