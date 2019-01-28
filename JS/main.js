3f51372e77107d5f14a4afdbaa6dd0bdb7a7d0d4
Kurt Heimbigner <10818122@uvu.edu>
2019-01-28 16:57:40 -0700

    Final updates

--- JS/main.js
+++ JS/main.js
@@ -1,6 +1,17 @@
 // JavaScript Document
-
+/*   jslint browser:true*/
 ///Adding in the header text
+
+"use strict";
+
 
 document.querySelector('#headerOne').textContent = "Joe's Bed and Breakfast";
 document.querySelector('#headerTwo').textContent = "Breakfast is Better in Bed";
+var dateGetter = new Date();
+var theDate = "Today's date is " + dateGetter;
+document.querySelector('#dateToday').innerHTML = theDate;
+
+var welcomeBox = prompt("Please enter your name", "Jane Doe");
+
+var welcomeAlert = "Welcome, " + welcomeBox + ", to Joe's Bed and Breakfast!";
+document.querySelector('#welcomeMsg').textContent = welcomeAlert;
